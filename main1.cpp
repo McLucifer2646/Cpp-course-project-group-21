8151b22c2b0f74b72b2fcc0a0e19765d95ae782d
McLucifer2646 <Anshul.Madurwar@iiitb.org>
2021-12-12 14:49:47 +0530

    FPS added

--- main1.cpp
+++ main1.cpp
@@ -3,13 +3,28 @@
 Game *g = nullptr;
 int main(int argc, const char *argv[])
 {
+    const int FPS = 60;
+    const int frameDelay = 1000/FPS;
+
+    Uint32 framestart;
+    int frameTime;
+
     g = new Game();
-    g->init("Mahabharat", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1000, 400, false);
+    g->init("Mahabharat", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720, false);
     while (g->running())
     {
+        framestart = SDL_GetTicks();
+
         g->handleEvents();
         g->update();
         g->render();
+
+        frameTime = SDL_GetTicks() - framestart;
+
+        if(frameDelay > frameTime)
+        {
+            SDL_Delay(frameDelay - frameTime);
+        }
     }
     g->clean();
     return 0;
